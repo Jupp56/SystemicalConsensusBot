45974b358c4c6212815cd60a25aba42122457834
Nick Grüßner <n.gruessner@t-online.de>
2019-05-27 14:06:33 +0200

    Projektdateien hinzufügen.

---
+++ SystemicalConsensusBot/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemicalConsensusBot
+{
+    public interface IDatabaseConnection
+    {
+        Poll GetPoll(int id);
+        Poll SavePoll(Poll poll);
+        List<Poll> GetPollsByOwner(int ownerID);
+    }
+}
---
+++ SystemicalConsensusBot/Poll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemicalConsensusBot
+{
+    public class Poll
+    {
+        public long PollID { get; internal set; } = -1;
+        public int OwnerId { get; }
+        public Dictionary<int, int[]> ParticipantVotes { get; } = new Dictionary<int, int[]>();
+
+        public Poll(int ownerId)
+        {
+            this.OwnerId = ownerId;
+        }
+
+    }
+}
